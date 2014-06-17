acf8fa0521ced274a0df4364bd38238664d7894c
tim <llStrainll@gmail.com>
2014-06-17 19:26:07 +0400

    add routing key param

--- lib/bunnytalk.ex
+++ lib/bunnytalk.ex
@@ -21,14 +21,14 @@ defmodule Bunnytalk.Publisher do
   use ExActor.GenServer
   import Exrabbit.Utils
 
-  def say(exchange, msg), do: say(exchange, msg, 0)
+  def say(exchange, msg, routing_key \\ ""), do: say(exchange, msg, routing_key, 0)
 
-  defp say(_exchange, _msg, attempts) when attempts > 10, do: :failed
-  defp say(exchange, msg, attempts) do
+  defp say(_exchange, _msg, _routing_key, attempts) when attempts > 10, do: :failed
+  defp say(exchange, msg, routing_key, attempts) do
     pid = :pg2.get_closest_pid :publishers
-    case call_publish(pid, exchange, msg) do
+    case call_publish(pid, exchange, msg, routing_key) do
       :ok -> :ok
-      _   -> say(exchange, msg, attempts + 1)
+      _   -> say(exchange, msg, routing_key, attempts + 1)
     end
   end
 
@@ -42,7 +42,7 @@ defmodule Bunnytalk.Publisher do
     initial_state(%{amqp: amqp, channel: channel})
   end
 
-  defcall call_publish(exchange, msg), state: %{amqp: _amqp, channel: channel} do
-    publish(channel, exchange, "", Jazz.encode!(msg), :wait_confirmation) |> reply
+  defcall call_publish(exchange, msg, routing_key), state: %{amqp: _amqp, channel: channel} do
+    publish(channel, exchange, routing_key, Jazz.encode!(msg), :wait_confirmation) |> reply
   end
 end
